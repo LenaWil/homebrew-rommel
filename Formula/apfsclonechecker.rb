919b892855693fef434226a9f473d7c4de673831
Lena Wildervanck <lena.wildervanck@gmail.com>
2024-02-13 12:18:58 +0100

    Update syntax

--- Formula/apfsclonechecker.rb
+++ Formula/apfsclonechecker.rb
@@ -1,15 +1,17 @@
 class Apfsclonechecker < Formula
-    desc "Utility to check if two files are clones in macOS APFS."
-    homepage "https://github.com/dyorgio/apfs-clone-checker"
-    url "https://github.com/dyorgio/apfs-clone-checker/archive/f7e6116fcf30ed51358ddd9edba6bb8327e61d17.tar.gz"
-    version "0.0.6"
-  
-    def install
-      system "cc", "clone_checker.c", "-O3", "-o", "clone_checker"
-      bin.install "./clone_checker" => "clone_checker"
-    end
-  
-    test do
-    end
+  desc "Utility to check if two files are clones in macOS APFS"
+  homepage "https://github.com/dyorgio/apfs-clone-checker"
+  url "https://github.com/dyorgio/apfs-clone-checker/archive/f7e6116fcf30ed51358ddd9edba6bb8327e61d17.tar.gz"
+  version "0.0.6"
+
+  # depends_on "gcc"
+
+  def install
+    system ENV.cc.to_s, "clone_checker.c", "-O3", "-o", "clone_checker"
+    bin.install "./clone_checker" => "clone_checker"
   end
-  
+
+  test do
+    return true
+  end
+end
